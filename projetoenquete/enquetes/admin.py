5a0405dfa3a1c8e2100fceb84a9b52c255a936e5
andre.almeida <andre.almeida@escolar.ifrn.edu.br>
2016-04-27 21:53:55 -0300

    Primeiro Projeto com Django

---
+++ projetoenquete/enquetes/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from . import models
+admin.site.register(models.Questao)
+admin.site.register(models.Opcao)
---
+++ projetoenquete/enquetes/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Questao(models.Model):
+    texto_questao=models.CharField("Texto da Questão",max_length=255)
+    data_publicacao=models.DateTimeField("Data da Publicação")
+    def __str__(self):
+        return self.texto_questao
+class Opcao(models.Model):
+    questao=models.ForeignKey(Questao,verbose_name="Questão")
+    texto_opcao=models.CharField("Texto",max_length=120)
+    votos=models.IntegerField("Número de Votos",default=0)
+    def __str__(self):
+        return self.questao.texto_questao + "-" + self.texto_opcao
