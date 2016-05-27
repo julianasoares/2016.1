08d1dc114f63e135f1660518f14477a257d37627
andre.almeida <andre.almeida@escolar.ifrn.edu.br>
2016-05-27 13:04:33 -0300

    Criação do modelo, banco de dados e arquivo para preencher o banco de dados.

--- projetovendas/appvendas/models.py
+++ projetovendas/appvendas/models.py
@@ -14,8 +14,12 @@ class Pessoa(models.Model):
     email=models.EmailField("E-Mail",max_length=200)
     telefone=models.CharField("Telefone",max_length=20)
 
+    def __str__(self):
+        return self.nome
+
 class Cliente(Pessoa):
     endereco=models.CharField("Endereço",max_length=255)
+
 
 class Funcionario(Pessoa):
     matricula=models.CharField("Matrícula",max_length=10)
@@ -25,3 +29,21 @@ class Produto(models.Model):
     descricao=models.CharField("Descrição",max_length=255)
     valorUnitario=models.DecimalField("Valor Unitário",max_digits=10,decimal_places=2)
     unidade=models.ForeignKey(Unidade,on_delete=models.PROTECT,verbose_name="Unidade")
+
+class Venda(models.Model):
+    dataVenda=models.DateField("Data da Venda")
+    vendedor=models.ForeignKey(Funcionario,on_delete=models.PROTECT,verbose_name="Funcionário")
+    cliente=models.ForeignKey(Cliente,on_delete=models.PROTECT,verbose_name="Cliente")
+    produtos=models.ManyToManyField(Produto,through="VendaProduto")
+
+    def calculaValorTotal(self):
+        lista=VendaProduto.objects.filter(venda=self) # Filtra os itens vinculados a esta venda
+        total=0
+        for item in lista: # Percorre a lista de itens, calculando o valor unitário
+            total=total + item.produto.valorUnitario * item.quantidade
+        return total
+
+class VendaProduto(models.Model):
+    venda=models.ForeignKey(Venda,on_delete=models.CASCADE)
+    produto=models.ForeignKey(Produto,on_delete=models.PROTECT)
+    quantidade=models.IntegerField("Quantidade")
