c4122cccc769e10ce93134dd9cbebcc6afcba361
andrealmeidaa <andre.almeida@escolar.ifrn.edu.br>
2016-06-10 11:27:56 -0300

    Implementação das views e templates básicos.

--- projetovendas/appvendas/views.py
+++ projetovendas/appvendas/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from appvendas.models import *
 # Create your views here.
+
+def produtos(request):
+    produtos=Produto.objects.all().order_by('descricao')
+    lista={'produtos':produtos}
+    return render(request,'produtos.html',lista)
+def unidades(request):
+    unidades=Unidade.objects.all().order_by('descricao')
+    lista={'unidades':unidades}
+    return render(request,'unidades.html',lista)
